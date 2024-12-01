42c16ac422080a3f602502ad78c3aa3da2b00dfb
Neal Vander Does <nvanderdoes@ivytech.edu>
2024-12-01 17:00:11 -0500

    Added textbox to eventually display stat information.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Author: Neal Vander Does
-Date: 11/22/2024
-Title: SDEV 220 Final Project: 
+Date: 12/1/2024
+Title: SDEV 220 Final Project: Packer Stats
 Class: SDEV220-50P-IO-202420-I-82X
 Desc: 
 """
@@ -24,22 +24,19 @@ ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("green")
 
 # Classes
-
 class GUI():
     def __init__(self):
-      
         # Creates Window
         self.root = ctk.CTk()
         self.root.title("Packer Stats")
         self.root.geometry("1280x720")
 
-
         # Configure GUI's grid
         self.root.grid_columnconfigure(1, weight=1)
         self.root.grid_columnconfigure((2, 3), weight=0) # Doesn't do anything?
-        self.root.grid_rowconfigure((0, 1), weight=1)
+        self.root.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # Creates sidebar with category buttons and an appearance mode menu
+        # Creates sidebar with category buttons and an appearance mode selector
         self.sidebar_frame = ctk.CTkFrame(self.root, width=200, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=6, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(6, weight=1)
@@ -62,7 +59,7 @@ class GUI():
         self.coaches_button = ctk.CTkButton(self.sidebar_frame, text="Coaching Staff", fg_color="#203731", font=ctk.CTkFont(size=20), command=self.coaches_positions)
         self.coaches_button.grid(row=5, column=0, padx=20, pady=10)
         
-        # Cool feature taken from a CTk example.
+        # Appearance mode selector
         self.appearance_mode_label = ctk.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=7, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = ctk.CTkOptionMenu(self.sidebar_frame, values=["System", "Light", "Dark"], command=self.change_appearance_mode_event)
@@ -72,34 +69,42 @@ class GUI():
         self.frame = ctk.CTkFrame(self.root, fg_color="transparent")
         self.frame.grid(row=0, column=1, sticky="nsew")
         self.frame.grid_columnconfigure(0, weight=1)
-        self.frame.grid_rowconfigure(1, weight=1)
+        self.frame.grid_rowconfigure((1, 2, 3), weight=1)
 
-        self.seg_button_1 = ctk.CTkSegmentedButton(self.frame, state="disabled", fg_color= "#FFB612", selected_color="#203731", font=ctk.CTkFont(size=20, weight="bold"))
-        self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(20, 10))
-        self.seg_button_1.configure(values=["Select a category on the left"])
+        self.seg_button = ctk.CTkSegmentedButton(self.frame, state="disabled", fg_color= "#FFB612", selected_color="#203731", font=ctk.CTkFont(size=20, weight="bold"))
+        self.seg_button.grid(row=0, column=0, padx=(20, 10), pady=(20, 0))
+        self.seg_button.configure(values=["Select a category on the left"])
 
-        self.root.mainloop()
+        # Textbox for stat info
+        self.textbox = ctk.CTkTextbox(self.root, state="disabled") # State breaks the default text
+        self.textbox.grid(row=0, rowspan=3, column=1, padx=(20, 15), pady=(75, 15), sticky="nsew")
+        self.textbox.insert("0.0", "Testing")
+
+        self.root.mainloop() # Starts the GUI
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         ctk.set_appearance_mode(new_appearance_mode)
     
     def o_positions(self):
-        self.seg_button_1.set(value="Quarterback")
-        self.seg_button_1.configure(values=["Quarterback", "Offensive Tackle", "Guard", "Center", "Wide Receiver", "Running Back", "Tight End"], state="normal", command="")
-            
+        self.seg_button.set(value="Quarterback")
+        self.seg_button.configure(values=["Quarterback", "Offensive Tackle", "Guard", "Center", "Wide Receiver", "Running Back", "Tight End"], state="normal", command="")
 
     def d_positions(self):
-        self.seg_button_1.set(value="Linebacker")
-        self.seg_button_1.configure(values=["Linebacker", "Cornerback", "Defensive Tackle", "Defensive End", "Safety"], state="normal", command="")
+        self.seg_button.set(value="Linebacker")
+        self.seg_button.configure(values=["Linebacker", "Cornerback", "Defensive Tackle", "Defensive End", "Safety"], state="normal", command="")
 
     def sp_positions(self):
-        self.seg_button_1.set(value="Place Kicker")
-        self.seg_button_1.configure(values=["Place Kicker", "Punter", "Long Snapper"], state="normal", command="")
+        self.seg_button.set(value="Place Kicker")
+        self.seg_button.configure(values=["Place Kicker", "Punter", "Long Snapper"], state="normal", command="")
 
     def coaches_positions(self):
-        self.seg_button_1.set(value="Temp")
-        self.seg_button_1.configure(values=["temporary value"], state="normal", command="")
+        self.seg_button.set(value="Temp")
+        self.seg_button.configure(values=["temporary value"], state="normal", command="")
 
+
+
+
+# Stat pulling
 class Green_Bay_Packers():
     pass
 
@@ -175,4 +180,4 @@ data = r2.json()["team"]["athletes"] # VERY IMPORTANT accesses 'athletes' sub in
 
 
 if __name__ == "__main__":
-    GUI() 
+    GUI() # Calls GUI Class
