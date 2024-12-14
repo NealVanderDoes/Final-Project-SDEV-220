5c5c2ddd8fbc6536351fb9aa5e2e58c021c1581c
Neal Vander Does <nvanderdoes@ivytech.edu>
2024-12-14 00:36:42 -0500

    Added stat pulling for all position group buttons.

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class GUI():
         # Creates sidebar with category buttons and an appearance mode selector
         self.sidebar_frame = ctk.CTkFrame(self.root, width=200, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=6, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(6, weight=1)
+        self.sidebar_frame.grid_rowconfigure(5, weight=1)
 
         self.logo_label = ctk.CTkLabel(self.sidebar_frame, text="Packer Stats", text_color="#FFB612", font=ctk.CTkFont(size=30, weight="bold", underline=True))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
@@ -57,14 +57,11 @@ class GUI():
         self.spt_button = ctk.CTkButton(self.sidebar_frame, text="Special Teams", fg_color="#203731", font=ctk.CTkFont(size=20), command=self.sp_positions)
         self.spt_button.grid(row=4, column=0, padx=20, pady=10)
         
-        self.coaches_button = ctk.CTkButton(self.sidebar_frame, text="Coaching Staff", fg_color="#203731", font=ctk.CTkFont(size=20), command=self.coaches_positions)
-        self.coaches_button.grid(row=5, column=0, padx=20, pady=10)
-        
         # Appearance mode selector
         self.appearance_mode_label = ctk.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_label.grid(row=6, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = ctk.CTkOptionMenu(self.sidebar_frame, values=["System", "Light", "Dark"], command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_optionemenu.grid(row=7, column=0, padx=20, pady=(10, 10))
 
         # Segmented Button config
         self.frame = ctk.CTkFrame(self.root, fg_color="transparent")
@@ -77,7 +74,7 @@ class GUI():
         self.seg_button.configure(values=["Select a category on the left"])
 
         # Textbox for stat info
-        self.textbox = ctk.CTkTextbox(self.root, state="disabled", font=ctk.CTkFont(size=35))  # State breaks the default text
+        self.textbox = ctk.CTkTextbox(self.root, state="disabled", font=ctk.CTkFont(size=20))  # State breaks the default text
         self.textbox.grid(row=0, rowspan=3, column=1, padx=(20, 15), pady=(75, 15), sticky="nsew")
 
         # Starts the GUI
@@ -86,106 +83,202 @@ class GUI():
     def change_appearance_mode_event(self, new_appearance_mode: str):
         ctk.set_appearance_mode(new_appearance_mode)
     
+    # Each function creates buttons in the GUI's segmented button
     def o_positions(self):
-        self.seg_button.configure(values=["Quarterback", "Offensive Tackle", "Guard", "Center", "Wide Receiver", "Running Back", "Tight End"], state="normal", command=self.get_seg_value)
+        self.seg_button.configure(values=["Quarterback", "Offensive Tackle", "Guard", "Center", "Wide Receiver", "Running Back", "Tight End"], state="normal", command=self.o_seg_value)
 
     def d_positions(self):
-        self.seg_button.set(value="Linebacker")
-        self.seg_button.configure(values=["Linebacker", "Cornerback", "Defensive Tackle", "Defensive End", "Safety"], state="normal", command="")
+        self.seg_button.configure(values=["Linebacker", "Cornerback", "Defensive Tackle", "Defensive End", "Safety"], state="normal", command=self.d_seg_value)
 
     def sp_positions(self):
-        self.seg_button.set(value="Place Kicker")
-        self.seg_button.configure(values=["Place Kicker", "Punter", "Long Snapper"], state="normal", command="")
+        self.seg_button.configure(values=["Place Kicker", "Punter", "Long Snapper"], state="normal", command=self.sp_seg_value)
 
-    def coaches_positions(self):
-        self.seg_button.set(value="Temp")
-        self.seg_button.configure(values=["temporary value"], state="normal", command="")
-
-    def get_seg_value(self, pos_name):
+    # Each function gets the exact name of the button pressed in the segmented button in order to pull that groups stats 
+    def o_seg_value(self, pos_name):
         self.seg_value = pos_name
         offense.o_players(self)
         return self.seg_value
     
-    # def update_textbox(self): # Possibly unnecessary
-    #     self.textbox.delete("0.0", "end")
-    #     self.textbox.insert("0.0", id_name)
+    def d_seg_value(self, pos_name):
+        self.seg_value = pos_name
+        defense.d_players(self)
+        return self.seg_value
     
+    def sp_seg_value(self, pos_name):
+        self.seg_value = pos_name
+        sp_teams.sp_players(self)
+        return self.seg_value
+        
 # Stat pulling
+class stats(GUI):
+    def offense_stats(self):      
+        # Filter for each offensive position groups stats (all but one oline stat is 0 so I didn't include them)
+        qb_stats = ["Games Plaed", "Completion Percentage", "Completions", "Interceptions", "Passing Touchdowns", "Passing Yards", "Total Sacks", 
+                        "Games Played", "Total Touchdowns", "Total Yards", "Yards Per Game", "Adjusted QBR"]
+        wr_te_stats = ["Games Played", "Long Reception", "Receiving First Downs", "Receiving Fumbles", "Receptions", "Total Touchdowns", "Receiving Yards", "Yards Per Game"]
+        rb_stats = ["Average Gain", "Long Rushing", "Rushing Yards", "Yards Per Game", "Rushing Attempts", "Rushing Fumbles", "Rushing Touchdowns", "Games Played"]
+        oline_stats = ["Games Played"]
+        
+        # Create a dictionary to store stats for each player
+        player_stats = {}
+
+        # Creates a link for every individual player in a position group and pulls some stats
+        web_format = map(lambda num: f"https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2024/types/2/teams/9/athletes/{num}/statistics", self.players.keys())
+        for link in web_format:
+            test_stats = requests.get(link)
+
+            if test_stats.status_code == 200: # Checks if the website call went well
+                test_stats_data = test_stats.json()["splits"]["categories"]
+                if self.seg_value == "Quarterback":
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"] 
+                                      == "general" or "passing" for nstat in stat["stats"] if nstat["displayName"] in qb_stats}
+                elif self.seg_value == "Wide Receiver":
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"] 
+                                      == "general" or "receiving" for nstat in stat["stats"] if nstat["displayName"] in wr_te_stats}
+                elif self.seg_value == "Running Back":
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"] 
+                                      == "general" or "rushing" for nstat in stat["stats"] if nstat["displayName"] in rb_stats}
+                elif self.seg_value == "Tight End":
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"]
+                                      == "general" or "receiving" for nstat in stat["stats"] if nstat["displayName"] in wr_te_stats}
+                else: 
+                    self.seg_value == "Offensive Tackle" or "Guard" or "Center"
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"]
+                                      == "general" for nstat in stat["stats"] if nstat["displayName"] in oline_stats}
+
+            # Adds the pulled stats to the player_stats dictionary with their name and id
+            player_name = self.players[link.split("/")[-2]] 
+            player_stats[player_name] = filtered_stats
+        return player_stats
+    
+    def defense_stats(self):
+        # Filter for each defensive position groups stats
+        lb_stats = ["Forced Fumbles", "Fumbles Recovered", "Interceptions", "Passes Defended", "Quarterback Hits", "Sacks", 
+                        "Games Played", "Stuffs", "Tackles For Loss", "Yards Per Game", "Total Tackles"]
+        cb_sfty_stats = ["Interceptions", "Passes Defended", "Sacks", "Solo Tackles", "Games Played"]
+        dt_de_stats = ["Quarterback Hits", "Sacks", "Hurries", "Stuffs", "Tackles For Loss", "Total Tackles", "Forced Fumbles", "Fumbles Recovered", "Games Played"]
+        
+        # Create a dictionary to store stats for each player
+        player_stats = {}
+
+        # Creates a link for every individual player in a position group and pulls some stats
+        web_format = map(lambda num: f"https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2024/types/2/teams/9/athletes/{num}/statistics", self.players.keys())
+        for link in web_format:
+            test_stats = requests.get(link)
+
+            if test_stats.status_code == 200: # Checks if the website call went well
+                test_stats_data = test_stats.json()["splits"]["categories"]
+                if self.seg_value == "Linebacker":
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"] 
+                                      == "general" or "defensive" or "defensiveInterceptions" for nstat in stat["stats"] if nstat["displayName"] in lb_stats}
+                elif self.seg_value == "Cornerback":
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"] 
+                                      == "general" or "defensive" or "defensiveInterceptions" for nstat in stat["stats"] if nstat["displayName"] in cb_sfty_stats}
+                elif self.seg_value == "Safety":
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"] 
+                                      == "general" or "defensive" or "defensiveInterceptions" for nstat in stat["stats"] if nstat["displayName"] in cb_sfty_stats}
+                else: 
+                    self.seg_value == "Defensive Tackle" or "Defensive End"
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"] 
+                                      == "general" or "defensive" for nstat in stat["stats"] if nstat["displayName"] in dt_de_stats}
+
+            # Adds the pulled stats to the player_stats dictionary with their name and id
+            player_name = self.players[link.split("/")[-2]] 
+            player_stats[player_name] = filtered_stats
+        return player_stats
+    
+    def sp_stats(self):
+        # Filter for each defensive position groups stats
+        pkicker_stats = ["Games Played", "Average Kickoff Return Yards", "Average Kickoff Yards", "Extra Point Attempts", "Extra Point Percentage", "Extra Point Blocked", 
+                         "Field Goal Attempts", "Field Goal Percentage", "Field Goals Blocked", "Long Field Goal Made"]
+        punter_stats = ["Games Played", "Punts", "Average Punt Return Yards", "Gross Average Punt Yards", "Long Punt", "Punt Returns", "Punt Return Yards", "Punts Blocked",
+                        "Punts Inside 10", "Punts Inside 20", "Punt Yards", "Touchbacks"]
+        lsnapper_stats = ["Games Played"]
+        
+        
+        # Create a dictionary to store stats for each player
+        player_stats = {}
+
+        # Creates a link for every individual player in a position group and pulls some stats
+        web_format = map(lambda num: f"https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2024/types/2/teams/9/athletes/{num}/statistics", self.players.keys())
+        for link in web_format:
+            test_stats = requests.get(link)
+
+            if test_stats.status_code == 200: # Checks if the website call went well
+                test_stats_data = test_stats.json()["splits"]["categories"]
+                if self.seg_value == "Place Kicker":
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"] 
+                                      == "general" or "kicking" for nstat in stat["stats"] if nstat["displayName"] in pkicker_stats}
+                elif self.seg_value == "Punter":
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"] 
+                                      == "general" or "punting" for nstat in stat["stats"] if nstat["displayName"] in punter_stats}
+                else: 
+                    self.seg_value == "Long Snapper"
+                    filtered_stats = {nstat["displayName"]:nstat["displayValue"] for stat in test_stats_data if stat["name"] 
+                                      == "general" for nstat in stat["stats"] if nstat["displayName"] in lsnapper_stats}
+
+            # Adds the pulled stats to the player_stats dictionary with their name and id
+            player_name = self.players[link.split("/")[-2]] 
+            player_stats[player_name] = filtered_stats
+        return player_stats
+    
 class offense(GUI):
     def o_players(self):
-
         self.textbox.configure(state="normal")
         self.textbox.delete("0.0", "end")
-        players = {name["id"]:name["fullName"] for name in data if name["status"]["id"] == "1" and name["position"]["name"] == self.seg_value} # Accesses ID's and Names
+        self.players = {name["id"]:name["fullName"] for name in data if name["status"]["id"] == "1" and name["position"]["name"] == self.seg_value}
 
-        id = [name["id"] for name in data if name["status"]["id"] == "1" and name["position"]["name"] == self.seg_value] # Just accesses ID's so I can create a format to scan many API links
-        print(id)
+        # Gets player stats
+        player_stats = stats.offense_stats(self)
 
-        for id_name in id:
-            self.textbox.insert("0.0", f"{id_name}\n")
+        # Displays player stats in the GUI textbox
+        for player_name, statistics in player_stats.items():
+            self.textbox.insert("end", f"{player_name}:\n")
+            for stat_name, stat_value in statistics.items():
+                self.textbox.insert("end", f" • {stat_name}: {stat_value}\n")
+
         self.textbox.configure(state="disabled")
-# Maybe use lambda for accessesing API with format?
+    
+class defense(GUI):
+    def d_players(self):
+        self.textbox.configure(state="normal")
+        self.textbox.delete("0.0", "end")
+        self.players = {name["id"]:name["fullName"] for name in data if name["status"]["id"] == "1" and name["position"]["name"] == self.seg_value}
 
+        # Gets player stats
+        player_stats = stats.defense_stats(self)
 
-# class defense(GUI):
-#     def d_players_stats(self):
+        # Displays player stats in the GUI textbox
+        for player_name, statistics in player_stats.items():
+            self.textbox.insert("end", f"{player_name}:\n")
+            for stat_name, stat_value in statistics.items():
+                self.textbox.insert("end", f" • {stat_name}: {stat_value}\n")
 
-#         players = {name["id"]:name["fullName"] for name in data if name["status"]["id"] == "1" and name["position"]["name"] in 
-#                    ["Linebacker", "Cornerback", "Defensive Tackle", "Defensive End", "Safety"]}
-        
-#         for value in players.values():
-#             print(value)
+        self.textbox.configure(state="disabled")
 
+class sp_teams(GUI):
+    def sp_players(self):
+        self.textbox.configure(state="normal")
+        self.textbox.delete("0.0", "end")
+        self.players = {name["id"]:name["fullName"] for name in data if name["status"]["id"] == "1" and name["position"]["displayName"] == self.seg_value}
 
-#         # print([value for value in players.values()]) # Could use list comprehension as well
-        
-#     def cornerback():
-#         pass
+        # Gets player stats
+        player_stats = stats.sp_stats(self)
 
-# class sp_teams(GUI):
-#     def sp_players():
-#         players = {name["id"]:name["fullName"] for name in data if name["status"]["id"] == "1" and name["position"]["name"] in 
-#                    ["Place kicker", "Punter", "Long Snapper"]}
-        
-#         for value in players.values():
-#             print(value)
+        # Displays player stats in the GUI textbox
+        for player_name, statistics in player_stats.items():
+            self.textbox.insert("end", f"{player_name}:\n")
+            for stat_name, stat_value in statistics.items():
+                self.textbox.insert("end", f" • {stat_name}: {stat_value}\n")
 
-#         # print([value for value in players.values()]) # Could use list comprehension as well
+        self.textbox.configure(state="disabled")   
 
-# class coaches(GUI):
-#     def coaching_staff():
-#         print("Coaching Staff: ")
-
-
-# # Data Fetching Sites
-# Ja_Stats = "https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2024/types/2/teams/9/athletes/3895429/statistics?lang=en&region=us"
+# Calls the website and pulls data for every player on the 53 man roster
 players = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/9?enable=roster,projection,stats"
 
-# # Accesses ESPN API website for individual stats & team information.
-# # r = requests.get(Ja_Stats)
+req = requests.get(players)
 
-r2 = requests.get(players)
-print(r2.status_code)
-
-data = r2.json()["team"]["athletes"] # VERY IMPORTANT accesses 'athletes' sub information within team info.
-
-# # # Dictionary comprehension detailing id and fullName for players IF they're on 52 man roster.
-# # athletes = {name["id"]:name["fullName"] for name in data if name["status"]["id"] == "1"}
-# # corners = {name["id"]:name["fullName"] for name in data if name["status"]["id"] == "1" and name["position"]["name"] == "Cornerback"} # Example how to access position grups
-# # print(corners)
-# # print(athletes)
-
-# # positions = {name["position"]["name"] for name in data if name["status"]["id"] == "1"}
-# # print(positions)
-
-
-# # with open("json_data.json", "wt") as data_file: 
-# #     # Probably temporary, wanted to make sure ALL the data was pulled correctly.
-# #     for line in r2.text:
-# #         data_file.write(line)
-
-
-
+data = req.json()["team"]["athletes"] # VERY IMPORTANT accesses 'athletes' sub information within team info.
 
 if __name__ == "__main__":
     GUI() # Calls GUI Class
